300ee3d8d8920dd8418570371ed8c742ec6b7d76
Umesh gupta <umeshgupta67867@gmmail.com>
2025-01-21 22:30:25 +0530

    dsfhk

--- todo-app/todo.js
+++ todo-app/todo.js
@@ -8,12 +8,35 @@ function AddTask(){
     else{
         let li= document.createElement("li");
         li.innerHTML= inputbox.value;
-        listcontainer.appendChild(li);
-        let span=document.createElement("li");
+        let span = document.createElement("span");
         span.innerHTML="\u00d7";
         li.appendChild(span);
+        listcontainer.appendChild(li);
+
+
 
     }
     inputbox.value="";
-
+    saveData();
 }
+
+ listcontainer.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+      else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+
+      }
+    },false);
+
+    function saveData(){
+        localStorage.setItem("data",listcontainer.innerHTML);
+
+    }
+    function showTask(){
+        listcontainer.innerHTML=localStorage.getItem("data");
+    }
+    showTask();
